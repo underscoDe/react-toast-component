85b8d3eae28b2fd45066b0ec3545b9b4b1e1ef29
Arsene Awounou <developer.awounou@gmail.com>
2023-02-05 20:58:30 +0100

    handle creation of multiple toasts with ToastShelf component

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -18,7 +18,7 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast({ variant, children, handleDismiss }) {
+function Toast({ variant, children, handleDismiss, id }) {
   const Icon = ICONS_BY_VARIANT[variant];
   
   return (
@@ -29,7 +29,7 @@ function Toast({ variant, children, handleDismiss }) {
       <p className={styles.content}>
         {children}
       </p>
-      <button onClick={handleDismiss} className={styles.closeButton}>
+      <button className={styles.closeButton} onClick={() => handleDismiss(id)}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,19 +1,47 @@
 import React from "react";
 
 import Button from "../Button";
-import Toast from "../Toast/Toast";
+import ToastShelf from "../ToastShelf";
 
 import styles from "./ToastPlayground.module.css";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
-  const [isRendered, setIsRendered] = React.useState(false);
+  const [toasts, setToasts] = React.useState([
+    {
+      id: crypto.randomUUID(),
+      message: "Message 1",
+      variant: "error",
+    },
+    {
+      id: crypto.randomUUID(),
+      message: "Message 2",
+      variant: "success",
+    },
+  ]);
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
   const [message, setMessage] = React.useState("");
 
-  const handleDismiss = () => {
-    setIsRendered((cur) => !cur);
+  function handleToastCreation(e) {
+    e.preventDefault();
+    const nextToast = [
+      ...toasts,
+      {
+        id: crypto.randomUUID(),
+        message,
+        variant,
+      },
+    ];
+    setToasts(nextToast);
+    setMessage("");
+    setVariant(VARIANT_OPTIONS[0]);
+  }
+
+  const handleDismiss = (id) => {
+    const nextToasts = toasts.filter((toast) => toast.id !== id);
+
+    setToasts(nextToasts);
   };
 
   return (
@@ -23,71 +51,62 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
-      {isRendered && (
+      {/* {isRendered && (
         <Toast variant={variant} handleDismiss={handleDismiss}>
           {message}
         </Toast>
-      )}
+      )} */}
+      <ToastShelf toasts={toasts} handleDismiss={handleDismiss} />
 
-      <div className={styles.controlsWrapper}>
-        <form>
-          <div className={styles.row}>
-            <label
-              htmlFor="message"
-              className={styles.label}
-              style={{ alignSelf: "baseline" }}
-            >
-              Message
-            </label>
-            <div className={styles.inputWrapper}>
-              <textarea
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                required={true}
-                id="message"
-                className={styles.messageInput}
-              />
-            </div>
+      <form onSubmit={handleToastCreation} className={styles.controlsWrapper}>
+        <div className={styles.row}>
+          <label
+            htmlFor="message"
+            className={styles.label}
+            style={{ alignSelf: "baseline" }}
+          >
+            Message
+          </label>
+          <div className={styles.inputWrapper}>
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required={true}
+              id="message"
+              className={styles.messageInput}
+            />
           </div>
+        </div>
 
-          <div className={styles.row}>
-            <div className={styles.label}>Variant</div>
-            <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-              {/* TODO Other Variant radio buttons here */}
-              {VARIANT_OPTIONS.map((option) => {
-                return (
-                  <label key={option} htmlFor={`variant-${option}`}>
-                    <input
-                      id={`variant-${option}`}
-                      type="radio"
-                      name="variant"
-                      value={option}
-                      checked={option === variant}
-                      onChange={(e) => setVariant(e.target.value)}
-                    />
-                    {option}
-                  </label>
-                );
-              })}
-            </div>
+        <div className={styles.row}>
+          <div className={styles.label}>Variant</div>
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            {/* TODO Other Variant radio buttons here */}
+            {VARIANT_OPTIONS.map((option) => {
+              return (
+                <label key={option} htmlFor={`variant-${option}`}>
+                  <input
+                    id={`variant-${option}`}
+                    type="radio"
+                    name="variant"
+                    value={option}
+                    checked={option === variant}
+                    onChange={(e) => setVariant(e.target.value)}
+                  />
+                  {option}
+                </label>
+              );
+            })}
           </div>
+        </div>
 
-          <div className={styles.row}>
-            <div className={styles.label} />
-            <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-              <Button
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleDismiss();
-                }}
-                type="submit"
-              >
-                Pop Toast!
-              </Button>
-            </div>
+        <div className={styles.row}>
+          <div className={styles.label} />
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            <Button type="submit">Pop Toast!</Button>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   );
 }
